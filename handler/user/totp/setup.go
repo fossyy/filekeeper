b890f5442ce50842f5b4f035bca3082791b41ba8
Bagas Aulia Rezki <bagas@fossy.my.id>
2024-06-19 18:43:33 +0700

    Improve 2FA setup page design

--- handler/user/totp/setup.go
+++ handler/user/totp/setup.go
@@ -4,82 +4,80 @@ import (
 	"bytes"
 	"encoding/base64"
 	"fmt"
-	"github.com/fossyy/filekeeper/db"
-	"github.com/fossyy/filekeeper/types"
-	"github.com/fossyy/filekeeper/utils"
 	userTotpSetupView "github.com/fossyy/filekeeper/view/user/totp"
-	"github.com/skip2/go-qrcode"
-	"github.com/xlzd/gotp"
 	"image/png"
 	"net/http"
 	"time"
+
+	"github.com/fossyy/filekeeper/db"
+	"github.com/fossyy/filekeeper/types"
+	"github.com/skip2/go-qrcode"
+	"github.com/xlzd/gotp"
 )
+
+func generateQRCode(uri string) (string, error) {
+	qr, err := qrcode.New(uri, qrcode.Medium)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate QR code: %w", err)
+	}
+
+	var buffer bytes.Buffer
+	if err := png.Encode(&buffer, qr.Image(256)); err != nil {
+		return "", fmt.Errorf("failed to encode QR code to PNG: %w", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(buffer.Bytes()), nil
+}
 
 func GET(w http.ResponseWriter, r *http.Request) {
 	secret := gotp.RandomSecret(16)
 	userSession := r.Context().Value("user").(types.User)
 	totp := gotp.NewDefaultTOTP(secret)
-	uri := totp.ProvisioningUri(userSession.Email, utils.Getenv("DOMAIN"))
-	qr, err := qrcode.New(uri, qrcode.Medium)
+	uri := totp.ProvisioningUri(userSession.Email, "filekeeper")
+	base64Str, err := generateQRCode(uri)
 	if err != nil {
-		fmt.Printf("Failed to generate QR code: %v", err)
+		fmt.Printf("%v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	var buffer bytes.Buffer
-	err = png.Encode(&buffer, qr.Image(256))
-	if err != nil {
-		fmt.Printf("Failed to encode QR code to PNG: %v", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	base64Str := base64.StdEncoding.EncodeToString(buffer.Bytes())
 
-	component := userTotpSetupView.Main("Totp setup page", base64Str, secret)
-	err = component.Render(r.Context(), w)
-	if err != nil {
+	component := userTotpSetupView.Main("Filekeeper - 2FA Setup Page", base64Str, secret, userSession)
+	if err := component.Render(r.Context(), w); err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 }
 
 func POST(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+	if err := r.ParseForm(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	code := r.Form.Get("totp")
 	secret := r.Form.Get("secret")
 	totp := gotp.NewDefaultTOTP(secret)
 	userSession := r.Context().Value("user").(types.User)
-	fmt.Println(userSession)
 	if totp.Verify(code, time.Now().Unix()) {
-		err := db.DB.InitializeTotp(userSession.Email, secret)
-		if err != nil {
+		if err := db.DB.InitializeTotp(userSession.Email, secret); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		fmt.Fprintf(w, "Authentication successful! Access granted.")
-		return
+		fmt.Fprint(w, "Authentication successful! Access granted.")
 	} else {
-		uri := totp.ProvisioningUri(userSession.Email, utils.Getenv("DOMAIN"))
-		qr, err := qrcode.New(uri, qrcode.Medium)
+		uri := totp.ProvisioningUri(userSession.Email, "filekeeper")
+
+		base64Str, err := generateQRCode(uri)
 		if err != nil {
-			fmt.Printf("Failed to generate QR code: %v", err)
+			fmt.Printf("%v\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		var buffer bytes.Buffer
-		err = png.Encode(&buffer, qr.Image(256))
-		if err != nil {
-			fmt.Printf("Failed to encode QR code to PNG: %v", err)
+
+		component := userTotpSetupView.Main("Filekeeper - 2FA Setup Page", base64Str, secret, userSession)
+		if err := component.Render(r.Context(), w); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		base64Str := base64.StdEncoding.EncodeToString(buffer.Bytes())
-		component := userTotpSetupView.Main("Totp setup page", base64Str, secret)
-		err = component.Render(r.Context(), w)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		return
 	}
 }
